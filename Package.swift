ca655c76d547d0d365abbb49c6c4718811ce1581
Swizzlr <git@swizzlr.co>
2017-01-29 19:25:34 -0500

    Update.

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@ import PackageDescription
 var package = Package(
   name: "TitanTopLevel",
   dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/bermudadigitalstudio/TitanRouter.git", majorVersion: 0, minor: 1)
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanRouter.git", majorVersion: 0, minor: 3)
   ]
 )
 
 // Uncomment the following to install sourcery for development
 //package.dependencies.append(
-//  .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.3")
+// .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.3")
 //)
